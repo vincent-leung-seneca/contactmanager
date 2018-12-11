862471399f63f93bb646b5c095d63e84a6865eed
vincent-leung-seneca <35344385+vincent-leung-seneca@users.noreply.github.com>
2018-12-11 09:17:25 -0500

    initial commit

---
+++ src/context.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+const Context = React.createContext();
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "DELETE_CONTACT":
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+        )
+      };
+    case "ADD_CONTACT":
+      return {
+        ...state,
+        contacts: [action.payload, ...state.contacts]
+      };
+
+    case "UPDATE_CONTACT":
+      return {
+        ...state,
+        contacts: state.contacts.map(contact =>
+          contact.id === action.payload.id
+            ? (contact = action.payload)
+            : contact
+        )
+      };
+    default:
+      return state;
+  }
+};
+
+export class Provider extends Component {
+  state = {
+    contacts: [
+      // {
+      //   id: 1,
+      //   name: "John Doe",
+      //   email: "jdoe@gmail.com",
+      //   phone: "555-555-5555"
+      // },
+      // {
+      //   id: 2,
+      //   name: "Karen Williams",
+      //   email: "karen@gmail.com",
+      //   phone: "222-222-2222"
+      // },
+      // {
+      //   id: 3,
+      //   name: "Henry Johnson",
+      //   email: "henry@gmail.com",
+      //   phone: "111-111-1111"
+      // }
+    ],
+    dispatch: action => {
+      this.setState(state => reducer(state, action));
+    }
+  };
+
+  async componentDidMount() {
+    // await waits for the response to finish
+    const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+    this.setState({ contacts: res.data });
+    //.then(res => this.setState({ contacts: res.data }));
+  }
+
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export const Consumer = Context.Consumer;

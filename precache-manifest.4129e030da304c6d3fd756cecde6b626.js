a469f53b3d01cc1e534f50e44525f27f192f9647
vincent-leung-seneca <35344385+vincent-leung-seneca@users.noreply.github.com>
2018-12-11 09:41:48 -0500

    Updates

---
+++ precache-manifest.4129e030da304c6d3fd756cecde6b626.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "05881a82a653760467f1",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "05881a82a653760467f1",
+    "url": "/contactmanager/static/js/main.05881a82.chunk.js"
+  },
+  {
+    "revision": "d3fddd30c5c74f92fb0f",
+    "url": "/contactmanager/static/css/1.ffd0e63b.chunk.css"
+  },
+  {
+    "revision": "d3fddd30c5c74f92fb0f",
+    "url": "/contactmanager/static/js/1.d3fddd30.chunk.js"
+  },
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "b36e3897c78c760ce39e69d1959072cd",
+    "url": "/contactmanager/index.html"
+  }
+];
